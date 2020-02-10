c788e31c88caf7655ad6917e39100920c107b2cf
Wederson Gianotto <wdgianotto@gmail.com>
2020-02-10 20:11:56 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/wederson/cursomc/domain/Categoria.java
+++ src/main/java/com/wederson/cursomc/domain/Categoria.java
@@ -1,5 +1,7 @@
 package com.wederson.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,7 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
--- src/main/java/com/wederson/cursomc/services/CategoriaService.java
+++ src/main/java/com/wederson/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.wederson.cursomc.services;
 
 import com.wederson.cursomc.domain.Categoria;
 import com.wederson.cursomc.repositories.CategoriaRepository;
+import com.wederson.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,9 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    public Categoria buscar(Integer id){
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = categoriaRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
