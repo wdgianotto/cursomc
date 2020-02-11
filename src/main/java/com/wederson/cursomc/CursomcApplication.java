aaf2c8ae31f363e4a76ae0fe28acff448b5273e7
Wederson Gianotto <wdgianotto@gmail.com>
2020-02-11 19:17:59 -0300

    Estado e cidade

--- src/main/java/com/wederson/cursomc/CursomcApplication.java
+++ src/main/java/com/wederson/cursomc/CursomcApplication.java
@@ -1,14 +1,19 @@
 package com.wederson.cursomc;
 
 import com.wederson.cursomc.domain.Categoria;
+import com.wederson.cursomc.domain.Cidade;
+import com.wederson.cursomc.domain.Estado;
 import com.wederson.cursomc.domain.Produto;
 import com.wederson.cursomc.repositories.CategoriaRepository;
+import com.wederson.cursomc.repositories.CidadeRepository;
+import com.wederson.cursomc.repositories.EstadoRepository;
 import com.wederson.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -19,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -42,5 +53,18 @@ public class CursomcApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+		Estado est1 = new Estado (null, "Minas Gerais");
+		Estado est2 = new Estado (null, "São Paulo");
+
+		Cidade c1 = new Cidade (null, "Uberlândia", est1);
+		Cidade c2 = new Cidade (null, "São Paulo", est2);
+		Cidade c3 = new Cidade (null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est1.getCidades().addAll(Arrays.asList(c2,c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 }
---
+++ src/main/java/com/wederson/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.wederson.cursomc.repositories;
+
+import com.wederson.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
