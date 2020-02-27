f8c2bd7900d06bbdad68b41655392b8225f98dda
Wederson Gianotto <wdgianotto@gmail.com>
2020-02-26 21:23:55 -0300

    Atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/wederson/cursomc/services/CategoriaService.java
+++ src/main/java/com/wederson/cursomc/services/CategoriaService.java
@@ -24,4 +24,9 @@ public class CategoriaService {
         obj.setId(null);
         return categoriaRepository.save(obj);
     }
+
+    public Categoria update(Categoria obj){
+        find(obj.getId());
+        return categoriaRepository.save(obj);
+    }
 }
