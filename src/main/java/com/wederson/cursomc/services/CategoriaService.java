73edb2bd5a1d4c58d539acfeb26ebce1684c00e4
Wederson Gianotto <wdgianotto@gmail.com>
2020-03-02 19:43:10 -0300

    Paginação com parâmetros opcionais na requisicao

--- src/main/java/com/wederson/cursomc/services/CategoriaService.java
+++ src/main/java/com/wederson/cursomc/services/CategoriaService.java
@@ -1,11 +1,15 @@
 package com.wederson.cursomc.services;
 
+import com.sun.javafx.scene.traversal.Direction;
 import com.wederson.cursomc.domain.Categoria;
 import com.wederson.cursomc.repositories.CategoriaRepository;
 import com.wederson.cursomc.services.exceptions.DataIntegrityException;
 import com.wederson.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -45,5 +49,10 @@ public class CategoriaService {
 
     public List<Categoria> findAll(){
         return categoriaRepository.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return categoriaRepository.findAll(pageRequest);
     }
 }
